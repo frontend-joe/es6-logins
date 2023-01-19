2db57f3735ce8937c460d7559040e181a3bf07b1
frontend-joe <frontendjoe@outlook.com>
2023-01-19 19:26:13 +0000

    adding signup-1

--- login-2/main.js
+++
@@ -1,5 +0,0 @@
-const togglePassword = (button) => {
-  button.classList.toggle("showing");
-  const input = document.getElementById("password");
-  input.type = input.type === "password" ? "text" : "password";
-};
